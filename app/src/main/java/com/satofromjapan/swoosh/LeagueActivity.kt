60f4995292a7bb4859726eb1e5b89e7a10368e23
Masato Takahashi <masato.takaha@gmail.com>
2017-12-13 14:42:13 -0800

    logged to see activity lifecycle

--- app/src/main/java/com/satofromjapan/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/satofromjapan/swoosh/LeagueActivity.kt
@@ -3,7 +3,7 @@ package com.satofromjapan.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
